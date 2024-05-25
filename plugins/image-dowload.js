562c8a4f1a806aac5e24b236b96af379576fee30
Ulquiorra <136200699+ulquio0rra@users.noreply.github.com>
2024-05-25 14:26:15 +0300

    Add files via upload

---
+++ plugins/image-dowload.js
@@ -0,0 +1,16 @@
+import { googleImage } from '@bochilteam/scraper'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) throw `*متنساش ال انت عاوز تبحث عليه يحب , مثال : .صوره لايت*`
+const res = await googleImage(text)
+let image = res.getRandom()
+let link = image
+conn.sendHydrated(m.chat, `🔎 *البحث:* ${text}
+🌎 *المصدر:* Google
+`, author, link, link, '🔗 اللينك', null, null, [
+['💫 التالي 💫', `/imagen ${text}`]
+], m)
+}
+handler.help = ['gimage <query>', 'imagen <query>']
+handler.tags = ['internet', 'tools']
+handler.command = /^(صوره|image|بحث-صورة)$/i
+export default handler
