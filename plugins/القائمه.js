e159b3ac1a9037c3a58d36aecc87940444025198
Ulquiorra <136200699+ulquio0rra@users.noreply.github.com>
2024-05-25 15:07:40 +0300

    Update القائمه.js

--- plugins/القائمه.js
+++ plugins/القائمه.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, isP
 try {
 let vn = './media/menu.mp3'
 let pp = imagen4
-let img = await(await fetch('https://telegra.ph/file/d7ae77d1178f9de50825c.jpg')).buffer()
+let img = await(await fetch('https://telegra.ph/file/5aa73a371ea32c99d3b4e.jpg')).buffer()
 let d = new Date(new Date + 3600000)
 let locale = 'ar'
 let week = d.toLocaleDateString(locale, { weekday: 'long' })
